aae2951142f6a6aa662bbbb343d908bb47863f4b
Ruan Failache <ruanfailache@gmail.com>
2023-11-15 18:52:33 -0300

    refactor(core): Configure error handler for bad request

--- src/main/java/dev/bank/api/core/config/ExceptionHandlerConfiguration.java
+++ src/main/java/dev/bank/api/core/config/ExceptionHandlerConfiguration.java
@@ -1,12 +1,18 @@
 package dev.bank.api.core.config;
 
 import dev.bank.api.core.exceptions.HttpRequestException;
-import dev.bank.api.core.exceptions.NotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @ControllerAdvice
 public class ExceptionHandlerConfiguration {
@@ -19,5 +25,17 @@ public class ExceptionHandlerConfiguration {
     @ExceptionHandler(HttpRequestException.class)
     public ResponseEntity<String> handleHttpRequestException(HttpRequestException e) {
         return ResponseEntity.status(e.getStatus()).body(e.getMessage());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ResponseEntity<Map<String, String>> handleValidationException(MethodArgumentNotValidException ex) {
+        BindingResult bindingResult = ex.getBindingResult();
+        List<FieldError> fieldErrors = bindingResult.getFieldErrors();
+
+        Map<String, String> errors = new HashMap<>();
+        fieldErrors.forEach(error -> errors.put(error.getField(), error.getDefaultMessage()));
+
+        return ResponseEntity.badRequest().body(errors);
     }
 }
